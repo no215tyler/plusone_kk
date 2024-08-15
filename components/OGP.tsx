ce247a5b75f92b9bab0e3ec5b3f3dbfcd00a7457
no215tyler <no.215.tyler@gmail.com>
2024-08-15 20:27:28 +0900

    ✨ OGP設定

---
+++ components/OGP.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const OGP = () => {
+  return (
+    <>
+        <meta property="og:title" content="Plus One Co.,Ltd." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://plusone-kk.vercel.app" />
+        <meta property="og:image" content="https://plusone-kk.vercel.app/plusone_logos/Plus-One.jpg" />
+        <meta property="og:description" content="前橋近郊リフォームのことならPlus One株式会社へお問い合わせください" />
+        <meta property="og:site_name" content="Plus One Co.,Ltd." />    
+    </>
+  )
+}
+
+export default OGP
