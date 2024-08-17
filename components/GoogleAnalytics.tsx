be1e53e5bbff4451ac4f98dd9490cc09e107a5fd
no215tyler <no.215.tyler@gmail.com>
2024-08-17 23:16:49 +0900

    ✨ Googleアナリティクス導入

---
+++ components/GoogleAnalytics.tsx
@@ -0,0 +1,29 @@
+import Script from "next/script";
+import React from "react";
+import * as gtag from "../lib/gtag.js";
+
+const GoogleAnalytics = () => {
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+ 
+           gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+           `,
+        }}
+      />
+    </>
+  );
+};
+
+export default GoogleAnalytics;
