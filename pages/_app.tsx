08019357fba57c91065d672ff0d2bbccbc90e2c5
no215tyler <no.215.tyler@gmail.com>
2024-08-17 23:16:49 +0900

    ✨ Googleアナリティクス導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 import "@/styles/globals.css";
+import * as gtag from "../lib/gtag";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouterChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.on("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
+  return (
+    <>
+      <GoogleAnalytics />
+      <Component {...pageProps} />
+    </>
+  );
 }
